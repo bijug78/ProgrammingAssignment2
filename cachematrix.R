ee02304612fc4afd52ba16039374ea86d76a924d
bijug78 <bijug78@gmail.com>
2015-12-22 00:39:20 -0600

    Second submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,22 @@
 # testable at command prompt as
-# a <- makeCacheMatrix() 
-# a$set(matrix(c(2,4,3,1,5,7,9,10,11),nrow=3,ncol=3))  
-# cacheSolve(a) ==> This is not from cache
-# cacheSolve(a) ==> This returns from cache
+# c <- makeCacheMatrix() 
+# c$set(matrix(c(2,4,3,1,5,7,9,10,11),nrow=3,ncol=3))  
+# cacheSolve(c) ==> This is not from cache
+# cacheSolve(c) ==> This returns from cache
 
 makeCacheMatrix <- function(x = matrix()) {
   # initialize to NULL
   cache <- NULL
   
-  # create the matrix in the working environment
+  # create the matrix here
   set <- function(y) {
     x <<- y
     cache <<- NULL
   }
   
-  # get the value of the matrix
+  # get the value of the matrix 
   get <- function() x
-  # invert the matrix and store in cache
+  # use inverse to get the inverse of the matrix
   setMatrix <- function(inverse) cache <<- inverse
   # get the inverted matrix from cache
   getInverse <- function() cache
@@ -29,23 +29,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## CacheSolve function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(param1, ...) {
   ## attempt to get the inverse of the matrix stored in cache
-  cache <- x$getInverse()
+  cache <- param1$getInverse()
   
   # return inverted matrix from cache if it exists
   # else create the matrix in working environment
   if (!is.null(cache)) {
-    message("getting cached data")
+    message("Inverted matrix from the Cache")
     
     # display matrix in console
     return(cache)
   }
   
   # create matrix since it does not exist
-  matrix <- x$get()
+  matrix <- param1$get()
   
-  # matrix needs to be square
+  # If the matrix is not a square error out
   
   tryCatch( {
     # set and return inverse of matrix
@@ -65,7 +65,7 @@ cacheSolve <- function(x, ...) {
   },
   finally = {
     # set inverted matrix in cache
-    x$setMatrix(cache)
+    param1$setMatrix(cache)
   } )
   
   # display matrix in console
